f5c80f4b19198995c243ca7e2de2a19873eedb17
NomadBlacky <hourou.hamurinzu@gmail.com>
2017-05-19 17:08:20 +0900

    Implement a function that convert markdown to html

--- index.md
+++ index.md
@@ -6,5 +6,5 @@ Twitter: [@blac_k_ey](https://twitter.com/blac_k_ey)
 
 ## Blogs
 
-+ [2017-05-11.md PostgreSQL - 権限の付与](blogs/2017-05-11.md)
-+ [2017-05-09.md はじめに](blogs/2017-05-09.md)
++ [2017-05-11.html <h1>PostgreSQL - 権限の付与</h1>](_build/2017-05-11.html)
++ [2017-05-09.html <h1>はじめに</h1>](_build/2017-05-09.html)
--- src/main/scala/org/nomadblacky/github/io/DeployBlogs.scala
+++ src/main/scala/org/nomadblacky/github/io/DeployBlogs.scala
@@ -3,6 +3,7 @@ package org.nomadblacky.github.io
 import java.io.FileNotFoundException
 
 import better.files._
+import org.pegdown.PegDownProcessor
 
 import scala.io.Source
 import scala.util.Try
@@ -12,14 +13,14 @@ import scala.util.Try
  */
 object DeployBlogs {
 
-  def getFile(path: String, isDirectory: Boolean): Try[File] = Try {
-    val f = File(path)
+  def getFile(pathFunc: ⇒ String, isDirectory: Boolean): Try[File] = Try {
+    val f = File(pathFunc)
     if (!f.exists) {
-      throw new FileNotFoundException(s"${path} is not found.")
+      throw new FileNotFoundException(s"${f.path} is not found.")
     }
     if (f.isDirectory ^ isDirectory) {
       val msg = if (f.isDirectory) "directory" else "file"
-      throw new IllegalStateException(s"${path} is a ${msg}")
+      throw new IllegalStateException(s"${f.path} is a ${msg}")
     }
     f
   }
@@ -28,13 +29,51 @@ object DeployBlogs {
     Source.fromURL(getClass.getResource("usage.txt")).getLines().mkString("\n")
   }
 
-  def buildBlogList(baseDir: File, currentDir: File = File(".")): String = {
-    baseDir
-      .listRecursively
+  def buildBlogListMarkDown(blogs: Seq[File], currentDir: File = File(".")): String = {
+    blogs
       .filter(!_.isDirectory)
       .map { f ⇒ (f, f.lines.take(1).map(_.replaceAll("""^#+\s+""", ""))) }
       .map { case (f, tr) ⇒ s"+ [${f.name} ${tr.mkString}](${currentDir.relativize(f).toString})" }
       .mkString("\n")
+  }
+
+  def writeIndex(blogs: Seq[File], templateFile: File, indexFile: File = File("index.md")) = {
+    val markdown =
+      templateFile.lines
+        .map { s ⇒
+          if (s.matches("""^::articles::$"""))
+            buildBlogListMarkDown(blogs)
+          else
+            s
+        }
+
+    indexFile
+      .createIfNotExists()
+      .clear()
+      .append(markdown.mkString("\n"))
+  }
+
+  def convertMarkdownToHtml(baseDir: File, destDir: File): List[File] = {
+    val markdowns = baseDir
+      .listRecursively
+      .filter(!_.isDirectory)
+      .filter(f ⇒ f.extension.map(_ == ".md").getOrElse(false))
+
+    val pegdown = new PegDownProcessor()
+
+    val pairs = markdowns
+      .map(f ⇒ (f, destDir / (f.nameWithoutExtension + ".html")))
+      .toList
+
+    destDir.createIfNotExists(true, true)
+    pairs.foreach {
+      case (from, to) ⇒
+        to.createIfNotExists()
+          .clear()
+          .write(pegdown.markdownToHtml(from.lines.mkString("\n")))
+    }
+
+    pairs.map(_._2).toList
   }
 
   def run(args: Array[String]): Unit = {
@@ -42,19 +81,8 @@ object DeployBlogs {
     val blogsDir = getFile(args(1), true).get
     val destDir = getFile(args(2), false).getOrElse(File("_build"))
 
-    val markdown =
-      templateFile.lines
-        .map { s ⇒
-          if (s.matches("""^::articles::$"""))
-            buildBlogList(blogsDir)
-          else
-            s
-        }
-
-    File("index.md")
-      .createIfNotExists()
-      .clear()
-      .append(markdown.mkString("\n"))
+    val markdowns = convertMarkdownToHtml(blogsDir, destDir)
+    writeIndex(markdowns, templateFile)
   }
 
   def main(args: Array[String]): Unit = {
